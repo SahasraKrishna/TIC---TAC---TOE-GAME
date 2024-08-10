902014ba2f47bb62867b2c22f4cfe38a99717626
SahasraKrishna <sahasrayaddala@gmail.com>
2024-08-11 00:14:18 +0530

    Add files via upload

---
+++ TIcTacToe.js
@@ -0,0 +1,76 @@
+let boxes=document.querySelectorAll(".box");
+let rstBtn=document.querySelector(".rst-btn");
+let newBtn=document.querySelector(".new-btn");
+let msgContainer=document.querySelector(".msg-container");
+let msg=document.querySelector("#msg");
+let turn0=true;
+
+const winningPattern=[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [3,4,5],
+    [6,7,8],
+    [2,4,6]
+];
+
+const resetGame = () =>{
+    turn0=true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+};
+
+boxes.forEach((box) =>{
+    box.addEventListener("click",()=>{
+    console.log("btn was clicked");
+    if(turn0){
+        box.innerText="O";
+        turn0=false;
+        //alert("turn of Player-X");
+    }
+    else{
+        box.innerText="X";
+        turn0=true;
+        //alert("turn of Player-O");
+    }
+    box.disabled=true;
+    checkWinner();
+});
+});
+
+const disableBoxes = () =>{
+    for(let box of boxes){
+        box.disabled=true;
+    }
+};
+
+const enableBoxes = () =>{
+    for(let box of boxes){
+        box.disabled=false;
+        box.innerText="";
+    }
+};
+const showWinner = (winner) =>{
+   msg.innerText = `Congartulations , winner is ${winner}`;
+   msgContainer.classList.remove("hide");
+   disableBoxes();
+};
+
+const checkWinner = () =>{
+    for(let pattern of winningPattern){
+        let pos1Val=boxes[pattern[0]].innerText;
+        let pos2Val=boxes[pattern[1]].innerText;
+        let pos3Val=boxes[pattern[2]].innerText;
+        if(pos1Val!="" && pos2Val!="" && pos3Val!=""){
+            if(pos1Val === pos2Val && pos2Val === pos3Val){
+                console.log("WINNER",pos1Val);
+                showWinner(pos1Val);
+            }
+        }
+
+    }
+};
+newBtn.addEventListener("click",resetGame);
+rstBtn.addEventListener("click",resetGame);
